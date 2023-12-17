c298e9a12d35460a1070c53f32155d51c348d449
Edward Stuart <edjstuart@gmail.com>
2023-12-17 19:07:46 +0000

    aoc 3 - further WIP

--- aoc3.js
+++ aoc3.js
@@ -7,7 +7,7 @@ fs.readFile("Input3.txt", (err, data) => {
   let specialChar = [];
   let specialCharNumbers = [];
   const inputs = data.toString().split("\n");
-  console.log(inputs);
+  //   console.log(inputs);
 
   //   inputs.forEach((input) => {
   //     [...input]
@@ -25,6 +25,73 @@ fs.readFile("Input3.txt", (err, data) => {
     specialChar = [];
     engineLine = [];
   });
-  console.log(engineNumbers);
-  console.log(specialCharNumbers);
+  //   console.log(engineNumbers);
+  //   console.log(specialCharNumbers);
+
+  engineNumbers.forEach((nums, lineIndex) => {
+    // console.log("nums", nums, "specchar", specialCharNumbers[lineIndex]);
+    nums.forEach((num) => {
+      if (
+        specialCharNumbers[lineIndex].includes(num) ||
+        specialCharNumbers[lineIndex + 1].includes(num)
+      ) {
+        console.log(
+          "same pos",
+          inputs[lineIndex],
+          specialCharNumbers[lineIndex],
+          lineIndex
+        );
+        if (lineIndex > 0 && specialCharNumbers[lineIndex - 1].includes(num)) {
+          console.log(
+            "same pos",
+            inputs[lineIndex],
+            specialCharNumbers[lineIndex],
+            lineIndex
+          );
+        }
+      }
+      if (
+        specialCharNumbers[lineIndex].includes(num + 1) ||
+        specialCharNumbers[lineIndex + 1].includes(num + 1)
+      )
+        console.log(
+          "to the right",
+          inputs[lineIndex],
+          specialCharNumbers[lineIndex],
+          lineIndex
+        );
+      if (
+        lineIndex > 0 &&
+        specialCharNumbers[lineIndex - 1].includes(num + 1)
+      ) {
+        console.log(
+          "same pos",
+          inputs[lineIndex],
+          specialCharNumbers[lineIndex],
+          lineIndex
+        );
+      }
+      if (
+        specialCharNumbers[lineIndex].includes(num - 1) ||
+        specialCharNumbers[lineIndex + 1].includes(num - 1)
+      )
+        console.log(
+          "to the left",
+          inputs[lineIndex],
+          specialCharNumbers[lineIndex],
+          lineIndex
+        );
+      if (
+        lineIndex > 0 &&
+        specialCharNumbers[lineIndex - 1].includes(num - 1)
+      ) {
+        console.log(
+          "same pos",
+          inputs[lineIndex],
+          specialCharNumbers[lineIndex],
+          lineIndex
+        );
+      }
+    });
+  });
 });
