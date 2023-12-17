d7d4edbd0301915ab495676b8cc84b1274d67c7b
Edward Stuart <edjstuart@gmail.com>
2023-12-17 22:20:51 +0000

    aoc 3.1

--- aoc3.js
+++ aoc3.js
@@ -1,97 +1,95 @@
 const fs = require("fs");
 
+const checkAdjacent = (specialCharNumbers, lineIndex, num, inputs, actual) => {
+  if (specialCharNumbers[lineIndex].includes(num)) {
+    return true;
+  }
+
+  if (lineIndex > 0 && specialCharNumbers[lineIndex - 1].includes(num)) {
+    return true;
+  }
+
+  if (
+    lineIndex < specialCharNumbers.length - 1 &&
+    specialCharNumbers[lineIndex + 1].includes(num)
+  ) {
+    return true;
+  }
+  return false;
+};
+
 fs.readFile("Input3.txt", (err, data) => {
   if (err) throw err;
   let engineLine = [];
   let engineNumbers = [];
   let specialChar = [];
   let specialCharNumbers = [];
-  const inputs = data.toString().split("\n");
-  //   console.log(inputs);
-
-  //   inputs.forEach((input) => {
-  //     [...input]
-  //   })
+  let trailingDigits = [];
+  let finalPartNumbers = [];
+  const inputs = data.toString().split(/\r?\n/);
 
   inputs.forEach((inputLine, index) => {
-    // engineLine.push([...input]);
     [...inputLine].forEach((char, charIndex) => {
       if (!isNaN(parseInt(char))) engineLine.push(charIndex);
       if (isNaN(parseInt(char)) && char === ".") engineLine.push(".");
-      if (isNaN(parseInt(char)) && char !== ".") specialChar.push(charIndex);
+      if (isNaN(parseInt(char)) && char !== ".") {
+        specialChar.push(charIndex);
+        engineLine.push(char);
+      }
     });
     engineNumbers[index] = engineLine;
     specialCharNumbers[index] = specialChar;
     specialChar = [];
     engineLine = [];
   });
-  //   console.log(engineNumbers);
-  //   console.log(specialCharNumbers);
 
   engineNumbers.forEach((nums, lineIndex) => {
-    // console.log("nums", nums, "specchar", specialCharNumbers[lineIndex]);
+    let isPartNumber = false;
+
     nums.forEach((num) => {
-      if (
-        specialCharNumbers[lineIndex].includes(num) ||
-        specialCharNumbers[lineIndex + 1].includes(num)
-      ) {
-        console.log(
-          "same pos",
-          inputs[lineIndex],
-          specialCharNumbers[lineIndex],
-          lineIndex
-        );
-        if (lineIndex > 0 && specialCharNumbers[lineIndex - 1].includes(num)) {
-          console.log(
-            "same pos",
-            inputs[lineIndex],
-            specialCharNumbers[lineIndex],
-            lineIndex
-          );
-        }
+      if (isNaN(parseInt(num)) && isPartNumber) {
+        let partNumberJoined = "";
+        trailingDigits.forEach((partNumIndex) => {
+          partNumberJoined += inputs[lineIndex][partNumIndex];
+        });
+        console.log(partNumberJoined);
+        finalPartNumbers.push(parseInt(partNumberJoined));
       }
-      if (
-        specialCharNumbers[lineIndex].includes(num + 1) ||
-        specialCharNumbers[lineIndex + 1].includes(num + 1)
-      )
-        console.log(
-          "to the right",
-          inputs[lineIndex],
-          specialCharNumbers[lineIndex],
-          lineIndex
-        );
-      if (
-        lineIndex > 0 &&
-        specialCharNumbers[lineIndex - 1].includes(num + 1)
-      ) {
-        console.log(
-          "same pos",
-          inputs[lineIndex],
-          specialCharNumbers[lineIndex],
-          lineIndex
-        );
+
+      if (!isNaN(parseInt(num))) {
+        trailingDigits.push(num);
+      } else if (isNaN(parseInt(num))) {
+        trailingDigits = [];
+        isPartNumber = false;
       }
-      if (
-        specialCharNumbers[lineIndex].includes(num - 1) ||
-        specialCharNumbers[lineIndex + 1].includes(num - 1)
-      )
-        console.log(
-          "to the left",
-          inputs[lineIndex],
-          specialCharNumbers[lineIndex],
-          lineIndex
+      let hasAdjacent = checkAdjacent(
+        specialCharNumbers,
+        lineIndex,
+        num,
+        inputs,
+        num
+      );
+
+      if (!hasAdjacent)
+        hasAdjacent = checkAdjacent(
+          specialCharNumbers,
+          lineIndex,
+          num + 1,
+          inputs,
+          num
         );
-      if (
-        lineIndex > 0 &&
-        specialCharNumbers[lineIndex - 1].includes(num - 1)
-      ) {
-        console.log(
-          "same pos",
-          inputs[lineIndex],
-          specialCharNumbers[lineIndex],
-          lineIndex
+
+      if (!hasAdjacent)
+        hasAdjacent = checkAdjacent(
+          specialCharNumbers,
+          lineIndex,
+          num - 1,
+          inputs,
+          num
         );
-      }
+
+      if (hasAdjacent && !isPartNumber) isPartNumber = true;
     });
   });
+  console.log(finalPartNumbers.reduce((a, b) => a + b, 0));
 });
